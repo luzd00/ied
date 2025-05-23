162b3cb9a45e07b0982da5fd11747fa7c82592f3
luzd00 <luizsalesguilherme@gmail.com>
2025-05-23 19:54:08 -0300

    Update arquiv.py
    melhorias

--- arquiv.py
+++ arquiv.py
@@ -1,4 +1,4 @@
-transporte =[]
+transporte = []
 nomes = []
 
 def adicionar_informaçao():
@@ -25,25 +25,18 @@ def adicionar_informaçao():
         print('lebre que so pode dijitar sim ou não')
 
 def ver_informaçao():
-
-  
-   
-   if not nomes:
-    print("dejete sua inforção")
-   else:
-   
-    for i, info in enumerate(nomes):
-        print("{i + 1}:")
-        print(f"nome: {info ['nome']}")
-        print(f"localização: {info ['tocalizacao']}")
-        print(f"idade: {info ['idade']}")
-    if i < len(transporte): 
-            print (f"transporte: {transporte[i]}")
-        
+    if not nomes:
+        print("dejete sua inforção")
     else:
-            print("cade o trasporte chapa")   
-        
-       
+        for i, info in enumerate(nomes):
+            print(f"{i + 1}:") 
+            print(f"nome: {info ['nome']}")
+            print(f"localização: {info ['localizacao']}")
+            print(f"idade: {info ['idade']}")
+            if i < len(transporte):
+                print(f"transporte: {transporte[i]}")
+            else:
+                print("cade o trasporte chapa")
 
 while True:
     print("1.adicionar tarefa")
@@ -54,11 +47,9 @@ while True:
 
     if opcao == "1":
         adicionar_informaçao()
-
     elif opcao == "2":
         ver_informaçao()
     elif opcao == "3":
-
         break
     else:
         print("redijite")
